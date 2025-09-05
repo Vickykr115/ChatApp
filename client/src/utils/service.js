93a878b6fc7d575f6775d648822ec50b9b7fb891
Alok-kumar-Official2 <kumaralok89938@gmail.com>
2025-09-05 14:22:17 +0530

    updated baseurl

--- client/src/utils/service.js
+++ client/src/utils/service.js
@@ -1,4 +1,5 @@
-export const baseUrl = "http://localhost:5000/api"
+// export const baseUrl = "http://localhost:5000/api"
+export const baseUrl = "https://chatapp-hjwq.onrender.com/api"
 
 export const postRequest = async (url, body) => {
     const response = await fetch(url, {
